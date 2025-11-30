b99771f7dc845cb5175eb82465e654ceec8bec79
aswinks123 <aswinks1995@outlook.com>
2025-11-30 00:14:25 -0500

    Remove pycache from repo

--- Helm-Complete-Projects/app.py
+++ Helm-Complete-Projects/app.py
@@ -8,10 +8,26 @@ app = FastAPI() #This creates your web application.
 
 @app.get("/") #When some one hit IP:def port, it reads the function read_root
 async def read_root():
-        return {"message": "Hello, World!"}
+        return {"message": "API Version : v1. Happy Learning!"}
 
 @app.get("/health") #A health check endpoint (very common in Kubernetes)
 async def health():
         return {"status": "ok"}
 
 
+# Requirements (fastapi, uvicorn) PIP modules
+
+# To run: uvicorn app:app --host 0.0.0.0 --port 8000 --reload
+
+"""
+uvicorn → runs the FastAPI server
+
+app:app → use the app object from app.py
+
+--host 0.0.0.0 → allow access from anywhere (needed for Docker/K8s)
+
+--port 8000 → run on port 8000
+
+--reload → auto-restart when code changes (for development)
+
+"""
